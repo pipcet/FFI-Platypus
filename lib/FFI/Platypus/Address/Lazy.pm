21b82b4d0b63b33905b8a2a3fd9eef7272e7ae9c
Philip <pipcet@gmail.com>
2015-03-07 23:46:54 +0000

    lazy "addresses". These break a bunch of tests for error conditions which we never reach because we don't realize the address.

---
+++ lib/FFI/Platypus/Address/Lazy.pm
@@ -0,0 +1,46 @@
+package FFI::Platypus::Address::Lazy;
+
+use strict;
+use warnings;
+use Carp qw(croak);
+use overload
+    bool => sub { my $self = shift; $self->realize },
+    '""' => sub { my $self = shift; $self->realize },
+    '0+' => sub { my $self = shift; $self->realize },
+    'cmp' => sub {
+      my($self, $other) = @_;
+      $self->realize cmp ($other->can('realize') ? $other->realize : $other);
+    },
+    '<=>' => sub {
+      my($self, $other) = @_;
+      $self->realize <=> ($other->can('realize') ? $other->realize : $other);
+    };
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $method = do { no warnings; no strict; $AUTOLOAD };
+
+  $method =~ s/.*:://;
+
+  $self->realize->$method(@_);
+}
+
+sub realize
+{
+  my($self) = @_;
+
+  return $self->{realization} if $self->{realization};
+
+  $self->{realization} = $self->{sub}->();
+
+  return $self->{realization};
+}
+
+sub new
+{
+  my($class, $sub) = @_;
+
+  return bless { sub => $sub }, $class;
+}
+
+1;
