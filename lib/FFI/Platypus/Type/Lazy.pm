175770c6f0ec801425b9947515e74d3eb482a882
Philip <pipcet@gmail.com>
2015-03-07 21:46:36 +0000

    FFI::Platypus::Type::Lazy->new($sub): a lazy type that will call $sub when it's actually needed. Slow, utterly non-magical implementation.

---
+++ lib/FFI/Platypus/Type/Lazy.pm
@@ -0,0 +1,36 @@
+package FFI::Platypus::Type::Lazy;
+
+use strict;
+use warnings;
+use Carp qw(croak);
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $method = do { no warnings; no strict; $AUTOLOAD };
+
+  $method =~ s/.*:://;
+
+  $self->realize->$method(@_);
+}
+
+sub realize
+{
+  my($self) = @_;
+
+  return $self->{realization} if $self->{realization};
+
+  $self->{realization} = $self->{sub}->();
+
+  croak 'failed to realize type' unless $self->{realization};
+
+  return $self->{realization};
+}
+
+sub new
+{
+  my($class, $sub) = @_;
+
+  return bless { sub => $sub }, $class;
+}
+
+1;
