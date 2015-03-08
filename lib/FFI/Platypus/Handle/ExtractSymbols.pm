96d41880d583d5546ba5bd6263d55ef1da9942a6
Philip <pipcet@gmail.com>
2015-03-08 02:03:18 +0000

    some resolver stubs

---
+++ lib/FFI/Platypus/Handle/ExtractSymbols.pm
@@ -0,0 +1,38 @@
+package FFI::Platypus::Handle::dl;
+use parent 'FFI::Platypus';
+
+use strict;
+use warnings;
+use 5.008001;
+use Carp qw( croak );
+use Scalar::Util qw( refaddr weaken );
+use Carp::Always;
+use FFI::ExtractSymbols;
+
+sub new
+{
+  my($class, $path) = @_;
+  my %symbols;
+
+  extract_symbols($path,
+      code => sub {
+	$symbols{$_[0]} = \@_;
+      });
+
+  return bless \%symbols, $class;
+}
+
+sub find_symbol
+{
+  my($self, $symbol) = @_;
+
+  return $self->{$symbol}->[2];
+}
+
+sub expensive
+{
+  'somewhat';
+}
+
+1;
+
---
+++ lib/FFI/Platypus/Handle/gdb.pm
@@ -0,0 +1,41 @@
+
+use parent 'FFI::Platypus';
+
+use strict;
+use warnings;
+use 5.008001;
+use Carp qw( croak );
+use Scalar::Util qw( refaddr weaken );
+use Carp::Always;
+use IPC::Run;package FFI::Platypus::Handle::dl;
+
+sub new
+{
+  my($class, $path) = @_;
+
+  my $self = bless { handle => FFI::Platypus::dl::dlopen() }, $class;
+
+  return $self;
+}
+
+sub find_symbol
+{
+  my($self, $symbol) = @_;
+
+  return FFI::platypus::dl::dlsym($self->{handle}, $symbol);
+}
+
+sub expensive
+{
+  'very';
+}
+
+sub DESTROY
+{
+  my($self) = @_;
+
+  FFI::Platypus::dl::dlclose($self->{handle});
+}
+
+1;
+
