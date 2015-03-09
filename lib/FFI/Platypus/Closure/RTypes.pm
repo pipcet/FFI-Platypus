e867e634d5a12fae451c0d2056c6915871c9c848
Philip <pipcet@gmail.com>
2015-03-09 23:42:27 +0000

    fix Closure::RTypes

---
+++ lib/FFI/Platypus/Closure/RTypes.pm
@@ -0,0 +1,7 @@
+package FFI::Platypus::Closure::RTypes;
+use parent -norequire, 'FFI::Platypus::Closure';
+
+sub add_data { FFI::Platypus::Closure::add_data(@_) }
+sub remove_data { FFI::Platypus::Closure::add_data(@_) }
+
+1;
