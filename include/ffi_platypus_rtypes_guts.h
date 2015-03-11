330318f8be4754cea9f1c85025275d43e26e95cb
Philip <pipcet@gmail.com>
2015-03-11 02:11:19 +0000

    fix warnings

--- include/ffi_platypus_rtypes_guts.h
+++ include/ffi_platypus_rtypes_guts.h
@@ -12,7 +12,7 @@ SV*  ffi_pl_rtypes_custom_array_perl(SV*,SV*,int);
 void ffi_pl_rtypes_custom_perl_cb(SV *, SV*, int);
 HV *ffi_pl_rtypes_ffi_get_type_meta(ffi_type *);
 HV *ffi_pl_rtypes_get_type_meta(SV *);
-HV *ffi_pl_ffi_get_type_meta(ffi_pl_type *); /* XXX no rtypes prefix */
+HV *ffi_pl_ffi_get_type_meta(ffi_type *); /* XXX no rtypes prefix */
 size_t ffi_pl_rtypes_sizeof(SV *,ffi_pl_rtypes_type *);
 void ffi_pl_rtypes_perl_complex_float(SV *sv, float *ptr);
 void ffi_pl_rtypes_perl_complex_double(SV *sv, double *ptr);
