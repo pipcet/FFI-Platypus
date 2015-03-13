a6166f494a3ae0cbf13a404150612b58d8454cfa
Philip <pipcet@gmail.com>
2015-03-13 02:05:26 +0000

    broken commit for record-keeping. We're running into a TCC bug where calling an integer constant cast to a function pointer will truncate it to 32 bits, resulting in a segfault unless the function happens to live in the lower 2 GB of memory address space.

--- lib/FFI/Platypus/RTypes/Type/FFI.pm
+++ lib/FFI/Platypus/RTypes/Type/FFI.pm
@@ -7,6 +7,40 @@ sub count_native_arguments
   1;
 }
 
+use Data::Dumper;
+
+sub perl_to_native_cexpr
+{
+  my($self, $expr) = @_;
+  my $meta = $self->meta;
+
+  warn Dumper($meta);
+
+  if($meta->{size} eq '8' and $meta->{sign}) {
+    return ['sint64_t', "SvIV(${expr})"];
+  } elsif($meta->{size} eq '4' and $meta->{sign}) {
+    return ['int', "SvIV(${expr})"];
+  }
+
+  return undef;
+}
+
+sub native_to_perl_cexpr
+{
+  my($self, $expr) = @_;
+  my $meta = $self->meta;
+
+  warn Dumper($meta);
+
+  if($meta->{size} eq '8' and $meta->{sign}) {
+    return ['sint64_t', "SvIV(${expr})"];
+  } elsif($meta->{size} eq '4' and $meta->{sign}) {
+    return ['int', "SvIV(${expr})"];
+  }
+
+  return undef;
+}
+
 sub DESTROY
 {
 }
