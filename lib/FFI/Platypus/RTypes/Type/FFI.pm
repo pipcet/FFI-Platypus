5d54ac40db250a8584364fc496a4f785499fe1eb
Philip <pipcet@gmail.com>
2015-03-13 02:39:47 +0000

    minor fix: Perl doesn't include ctypes

--- lib/FFI/Platypus/RTypes/Type/FFI.pm
+++ lib/FFI/Platypus/RTypes/Type/FFI.pm
@@ -12,8 +12,8 @@ sub perl_to_native_cexpr
   my($self, $expr) = @_;
   my $meta = $self->meta;
 
-  if($meta->{size} eq '8' and $meta->{sign}) {
-    return ['sint64_t', "SvIV(${expr})"];
+  if($meta->{size} eq '8') {
+    return ['long int', "SvIV(${expr})"];
   } elsif($meta->{size} eq '4' and $meta->{sign}) {
     return ['int', "SvIV(${expr})"];
   }
